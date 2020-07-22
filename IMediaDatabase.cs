c70d1be1a0bb97baa1afeb2f593d59be672a539e
Omid Manikhi <omm@travelonium.com>
2020-07-22 17:31:28 +0200

    Instantiated the MediaDatabase and also extracted its interface

---
+++ IMediaDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace MediaCurator
+{
+   public interface IMediaDatabase
+   {
+      string FullPath { get; }
+
+      void UpdateDatabase();
+      void UpdateMedia(XElement element, IProgress<Tuple<double, double>> progress, IProgress<byte[]> preview);
+      MediaFile InsertMedia(string path, IProgress<Tuple<double, double>> progress, IProgress<byte[]> preview);
+   }
+}
