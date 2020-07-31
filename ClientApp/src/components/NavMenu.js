fbe4dc6bba1253f2ff4b478512b99077df6dbc1e
Omid Manikhi <omm@travelonium.com>
2020-07-31 18:16:52 +0200

    Removed the media library link from the navigation bar for now

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -31,9 +31,6 @@ export class NavMenu extends Component {
                         <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
                             <ul className="navbar-nav flex-grow">
                                 <NavItem>
-                                    <NavLink tag={Link} className="text-dark" to="/">Library</NavLink>
-                                </NavItem>
-                                <NavItem>
                                     <NavLink tag={Link} className="text-dark" to="/settings">Settings</NavLink>
                                 </NavItem>
                             </ul>
