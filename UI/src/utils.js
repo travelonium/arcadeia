ad003a127d18839099366174637bf30ed53fff8d
Omid Manikhi <omm@travelonium.com>
2020-08-21 18:34:24 +0200

    Fixed an unlikely case in extract where the obj is null

--- UI/src/utils.js
+++ UI/src/utils.js
@@ -1,7 +1,7 @@
 ﻿export function extract(fallback, obj, level, ...rest) {
-    if (obj === undefined) return fallback
-    if (rest.length === 0 && obj.hasOwnProperty(level)) return obj[level]
-    return extract(fallback, obj[level], ...rest)
+    if ((obj === undefined) || (obj === null)) return fallback;
+    if (rest.length === 0 && obj.hasOwnProperty(level)) return obj[level];
+    return extract(fallback, obj[level], ...rest);
 }
 
 export function duration(seconds) {
