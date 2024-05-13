c95ecb5aff3e69dc55ea6de4be45ca6bdb0afe69
Omid Manikhi <omm@travelonium.com>
2024-05-13 12:34:36 +0200

    Added support for dragging and dropping links to files.

--- UI/src/components/UploadZone.js
+++ UI/src/components/UploadZone.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { toast } from 'react-toastify';
 import cx from 'classnames';
 import './UploadZone.scss';
 
@@ -13,6 +14,27 @@ export class UploadZone extends Component {
         };
     }
 
+    isValidHttpUrl(string) {
+        let url;
+        try {
+            url = new URL(string);
+        } catch (_) {
+            return false;
+        }
+        return url.protocol === "http:" || url.protocol === "https:";
+    }
+
+    renderErrorToast(title, subtitle) {
+        return (
+            <>
+                <div>
+                    <strong>{title}</strong>
+                </div>
+                <small>{subtitle}</small>
+            </>
+        );
+    }
+
     onChange(event) {
         event.preventDefault();
     }
@@ -23,8 +45,8 @@ export class UploadZone extends Component {
         let dragging = this.state.dragging;
         // are file(s) being dragged or else?
         if (event.dataTransfer.types) {
-            for (var i=0; i<event.dataTransfer.types.length; i++) {
-                if (event.dataTransfer.types[i] === "Files") {
+            for (const type of event.dataTransfer.types) {
+                if ((type === "Files") || (type.match('^text/plain'))) {
                     dragging = true;
                 }
             }
@@ -42,7 +64,56 @@ export class UploadZone extends Component {
         this.setState({
             dragging: false
         }, () => {
-            let files = Array.from(event.dataTransfer.files);
+            const dataTransfer = event.dataTransfer;
+            for (const item of dataTransfer.items) {
+                if (item.kind === 'string' && item.type.match('^text/plain')) {
+                    // If the item is the URL of the image
+                    item.getAsString(async (url) => {
+                        if (!this.isValidHttpUrl(url)) return;
+                        try {
+                            const response = await fetch(url);
+                            const blob = await response.blob();
+                            // MIME to extension mapping for images and videos
+                            const mimeExtension = {
+                                'image/jpeg': '.jpg',
+                                'image/png': '.png',
+                                'image/gif': '.gif',
+                                'image/bmp': '.bmp',
+                                'image/svg+xml': '.svg',
+                                'image/webp': '.webp',
+                                'image/tiff': '.tiff',
+                                'image/heic': '.heic',
+                                'video/mp4': '.mp4',
+                                'video/mpeg': '.mpeg',
+                                'video/quicktime': '.mov',
+                                'video/webm': '.webm',
+                                'video/ogg': '.ogv',
+                                'video/x-msvideo': '.avi',
+                                'video/x-ms-wmv': '.wmv',
+                                'video/3gpp': '.3gp',
+                                'video/3gpp2': '.3g2',
+                                'video/x-matroska': '.mkv'
+                            };
+                            // Attempt to extract a filename from the URL
+                            const urlComponents = new URL(url);
+                            let filename = urlComponents.pathname.split('/').pop();
+                            // Check if URL derived filename seems valid
+                            if (!filename.includes('.')) {
+                                // If no extension in filename, append one based on MIME type
+                                const defaultExt = mimeExtension[blob.type] || '.bin';
+                                filename += defaultExt;
+                            }
+                            let file = new File([blob], filename, { type: blob.type });
+                            if (this.props.onUpload !== undefined) this.props.onUpload([file]);
+                        } catch (error) {
+                            let title = "Fetch Error";
+                            console.error("Error fetching or processing the dropped link:", error.message);
+                            toast.error(this.renderErrorToast(title,  error.message));
+                        }
+                    });
+                }
+            }
+            let files = Array.from(dataTransfer.files || []);
             if (this.props.onUpload !== undefined) this.props.onUpload(files);
         });
     }
