586ee136eb62d969485ad4eb48389dd8a796c507
Omid Manikhi <omm@travelonium.com>
2025-02-28 16:11:38 +0100

    If the extracted MedifaFolder date is invalid i.e. is equal to epoch origin, set it to null

--- MediaFolder.cs
+++ MediaFolder.cs
@@ -1,21 +1,21 @@
-/* 
+/*
  *  Copyright © 2024 Travelonium AB
- *  
+ *
  *  This file is part of Arcadeia.
- *  
+ *
  *  Arcadeia is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
  *  by the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  Arcadeia is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *  GNU Affero General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU Affero General Public License
  *  along with Arcadeia. If not, see <https://www.gnu.org/licenses/>.
- *  
+ *
  */
 
 using Microsoft.Extensions.Options;
@@ -70,8 +70,8 @@ namespace Arcadeia
             // Acquire the common directory information.
             DirectoryInfo directoryInfo = new(FullPath);
 
-            DateCreated = directoryInfo.CreationTimeUtc;
-            DateModified = directoryInfo.LastWriteTimeUtc;
+            DateCreated = directoryInfo.CreationTimeUtc != DateTimeOffset.FromUnixTimeSeconds(0).UtcDateTime ? directoryInfo.CreationTimeUtc : null;
+            DateModified = directoryInfo.LastWriteTimeUtc != DateTimeOffset.FromUnixTimeSeconds(0).UtcDateTime ? directoryInfo.LastWriteTimeUtc : null;
          }
          catch (Exception e)
          {
